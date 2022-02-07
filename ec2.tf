3cd860385c3f838ed5be38a41ef9a17a13f037d5
maks1111148 <37628393+maks1111148@users.noreply.github.com>
2022-02-07 10:34:11 +0200

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = "AKIAVONMOGHPDR73XBSR"
+  secret_key = "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"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "admin1"
+  }
+}
